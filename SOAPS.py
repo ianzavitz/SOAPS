19fd4dfb53ceb40dca0a3923830d9b40ce55b3be
Ian Zavitz <31973908+ianzavitz@users.noreply.github.com>
2018-05-04 11:21:13 -0500

    Update SOAPS.py

--- SOAPS.py
+++ SOAPS.py
@@ -117,17 +117,17 @@ if(megahit):
 
 if(idba):
     if(len(fwd)==1 and len(rev)==1):
-        with gzip.open(fwd[0],'r') as f:
-            with open(fwd[0].replace(".gz",""),'w') as o:
-                for line in f:
-                    o.write(line.decode() )
-                o.close()
-        with gzip.open(rev[0],'r') as f:
-            with open(rev[0].replace(".gz",""),'w') as o:
-                for line in f:
-                    o.write(line.decode())
-                o.close()
-        fq2faRun += os.getcwd()+ "/idba/bin/fq2fa --merge "+fwd[0].replace(".gz","")+" "+rev[0].replace(".gz","")+" "+out+"/idba/idba_merged.fa"
+        #with gzip.open(fwd[0],'r') as f:
+         #   with open(fwd[0].replace(".gz",""),'w') as o:
+          #      for line in f:
+           #         o.write(line.decode() )
+            #    o.close()
+        #with gzip.open(rev[0],'r') as f:
+         #   with open(rev[0].replace(".gz",""),'w') as o:
+          #      for line in f:
+           #         o.write(line.decode())
+            #    o.close()
+        fq2faRun += os.getcwd()+ "/idba/bin/fq2fa --merge "+fwd[0]+" "+rev[0]+" "+out+"/idba/idba_merged.fa"
         print("\nfq2fa(IDBA)\n"+fq2faRun)
         os.system(fq2faRun)
     
